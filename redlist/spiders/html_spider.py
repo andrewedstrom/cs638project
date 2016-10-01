0f23044f1517c8c15213e8cdb7cb3eb1e8b42fe0
andrewedstrom <andrewedstrom@gmail.com>
2016-09-30 21:18:36 -0500

    ACTUALLY SCRAPING REDLISTgit add redlist/settings.pygit add redlist/settings.pygit add redlist/settings.py

--- redlist/spiders/html_spider.py
+++ redlist/spiders/html_spider.py
@@ -6,7 +6,7 @@ class RedlistHtmlSpider(scrapy.Spider):
 
     # Initiate a request and yield an iterable of request
     def start_requests(self):
-        urlfile = 'redlist-misc/urls'
+        urlfile = 'redlist-endangered-misc/urls'
         f = open(urlfile, 'r')
         urls = f.read().split('\n')
         for url in urls:
@@ -16,8 +16,8 @@ class RedlistHtmlSpider(scrapy.Spider):
     # Saves raw html and finds new URLs to follow
     def parse(self, response):
         # Save raw html
-        animal = response.url.split('/')[-2]
-        filename = 'redlist-html/%s.html' % animal
+        animalnum = response.url.split('/')[-2]
+        filename = 'redlist-endangered-html/%s.html' % animalnum
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log('Crikey! Looks like we caught us a wild %s!' % animal)
+        # self.log('Crikey! Looks like we caught us a wild %s!' % animal)
--- redlist/spiders/link_spider.py
+++ redlist/spiders/link_spider.py
@@ -4,6 +4,8 @@ class RedlistLinkSpider(scrapy.Spider):
     # Unique identifier for spider
     name = 'redlist-link'
 
+    # knabbedcount = 0
+
     # Initiate a request and yield an iterable of request
     def start_requests(self):
         urls = []
@@ -11,14 +13,20 @@ class RedlistLinkSpider(scrapy.Spider):
         for i in range(2,141):
             urls.append("http://www.iucnredlist.org/search?page={0}".format(i))
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url,
+                                 cookies={'user_choice2':'return',
+                                          '_newiucnredlist_session' : 'cebe1029de27b7b51c463650bd2f11ac;',
+                                          '_gat':'1',
+                                          '_ga': 'GA1.2.1229546810.1474666915'},
+                                 callback=self.parse)
 
     # Handle response downloaded for each request
     # Saves raw html and finds new URLs to follow
     def parse(self, response):
-        htmllist = response.xpath('//li/h2/a/@href').extract()
-        filename = 'arkive-endangered-misc/urls'
+        urlstublist = response.xpath('//li[(((count(preceding-sibling::*) + 1) > 0) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "desc", " " ))]').css('a.title::attr(href)').extract()
+        # knabbedcount += len(urlstublist)
+        filename = 'redlist-endangered-misc/urls'
         with open(filename, 'ab') as f:
-            for html in htmllist:
-                f.write("http://www.arkive.org" + html + "factsheet\n")
-        self.log('Knabbed urls from page %s' % response.url.split('/')[-1])
+            for urlstub in urlstublist:
+                f.write('http://www.iucnredlist.org' + urlstub + '\n')
+        # self.log('Number of URLs knabbed thus far = ', knabbedcount)
