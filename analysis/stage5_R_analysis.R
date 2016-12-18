99ef308d6c1930297cb57bc95dca4241b3f15293
Chris Endemann <endemann@wisc.edu>
2016-12-17 20:06:00 -0800

    finalized anlaysis file with linear regress, correlation matrix, and chi-square analysis.  Plot and table code included

--- analysis/stage5_R_analysis.R
+++ analysis/stage5_R_analysis.R
@@ -6,167 +6,123 @@ d <- read.csv("labeled.csv")
 varDescribe(d)
 str(d)
 
+# convert popTrend to be a continuous var --> add order to levels so it makes sense
+# first convert Unknown field to NA's for cleaner correlation values
+levels(d$ltable_pop_trend)
+d$new_pop_trend = d$ltable_pop_trend
+d[d[,c('new_pop_trend')] == "Unknown",c('new_pop_trend')] = NA
+d[d[,c('new_pop_trend')] == "",c('new_pop_trend')]= NA
+d[d[,c('new_pop_trend')] == "" & !is.na(d[,c('new_pop_trend')]),c('new_pop_trend')] = NA
+levels(d$new_pop_trend)
 
-# maxtrix?
-chisq.test(as.matrix(d[,6,7]))
-dMat <- as.matrix(d[,6:7])
-chisq.test(d$ltable_genus,d$ltable_status) # this works
+d$new_pop_trend = as.character(d$new_pop_trend)
+str(d$new_pop_trend)
+d[d[,c('new_pop_trend')] == "Unknown",c('new_pop_trend')]
+d$new_pop_trend <- as.factor(d$new_pop_trend)
+str(d$new_pop_trend)
+#d$new_pop_trend <- ordered(d$new_pop_trend, levels = c("Unknown","Decreasing", "Increasing"))
 
-# table?
-# see if we can use only rows with all data?
-table(d[,c('ltable_family','ltable_genus')])
-d$ltable_family
-d$ltable_genus
+# select ordinal vars for correlation matrix
+ordinalVarKeysToLookAt <- c('length','ltable_status','new_pop_trend','ltable_country_count','rtable_tCount')
+#install.packages("sjPlot")
+library(sjPlot)
 
+dSubset <- d[,ordinalVarKeysToLookAt]
+# rename some columns for prettiness sake
+colnames(dSubset) <- c("length","status","popTrend",'countryCount','threatCount')
+dSubset[] <- lapply(dSubset,as.integer)
+dSubset
 
-chisq.test(table(d[,6,7]))
-tableVersion <- table(d[,6:7])
-str(tableVersion)
-d[,7]
-summary(tableVersion)
-str(d)
-# Univariate exploration
-varPlot(d$rtable_country_count) 
-chisq.test(d)
+# make things a bit prettier
+sjp.setTheme(theme.font = NULL,
+             title.color = "black", title.size = 1.2, title.align = "left",
+             title.vjust = NULL, geom.outline.color = NULL, geom.outline.size = 0,
+             geom.boxoutline.size = 0.5, geom.boxoutline.color = "black",
+             geom.alpha = 1, geom.linetype = 1, geom.errorbar.size = 0.7,
+             geom.errorbar.linetype = 1, geom.label.color = NULL,
+             geom.label.size = 10, geom.label.alpha = 1, geom.label.angle = 0,
+             axis.title.color = "grey30", axis.title.size = 1.1,
+             axis.title.x.vjust = NULL, axis.title.y.vjust = NULL, axis.angle.x = 0,
+             axis.angle.y = 0, axis.angle = NULL, axis.textcolor.x = "black",
+             axis.textcolor.y = "black", axis.textcolor = NULL,
+             axis.linecolor.x = NULL, axis.linecolor.y = NULL, axis.linecolor = NULL,
+             axis.line.size = 0.5, axis.textsize.x = 1, axis.textsize.y = 1,
+             axis.textsize = NULL, axis.tickslen = NULL, axis.tickscol = NULL,
+             axis.ticksmar = NULL, axis.ticksize.x = NULL, axis.ticksize.y = NULL,
+             panel.backcol = NULL, panel.bordercol = NULL, panel.col = NULL,
+             panel.major.gridcol = NULL, panel.minor.gridcol = NULL,
+             panel.gridcol = NULL, panel.gridcol.x = NULL, panel.gridcol.y = NULL,
+             panel.major.linetype = 1, panel.minor.linetype = 1, plot.backcol = NULL,
+             plot.bordercol = NULL, plot.col = NULL, plot.margins = NULL,
+             legend.pos = "right", legend.just = NULL, legend.inside = FALSE,
+             legend.size = 1, legend.color = "black", legend.title.size = 1,
+             legend.title.color = "black", legend.title.face = "bold",
+             legend.backgroundcol = "white", legend.bordercol = "white",
+             legend.item.size = NULL, legend.item.backcol = "grey90",
+             legend.item.bordercol = "white")
 
-#############################################
-########### correlate some shit #############
-#############################################
-
-#### feats to use?
-# create numeric representation of factor variables
-d$rtable_status_NUM <- as.numeric(d$rtable_status)
-d$rtable_name_NUM <- as.numeric(d$rtable_name)
-d$rtable_kingdom_NUM <- as.numeric(d$rtable_kingdom)
-d$rtable_phylum_NUM <- as.numeric(d$rtable_phylum)
-d$rtable_class_NUM <- as.numeric(d$rtable_class)
-d$rtable_order_NUM <- as.numeric(d$rtable_order)
-d$rtable_family_NUM <- as.numeric(d$rtable_family)
-d$rtable_genus_NUM <- as.numeric(d$rtable_genus)
-d$rtable_size_NUM <- as.numeric(d$rtable_size)
-d$rtable_threat_keywords_NUM <- as.numeric(d$rtable_threat_keywords)
-d$rtable_conservation_keywords_NUM <- as.numeric(d$rtable_conservation_keywords)
-d$rtable_countries <- as.numeric(d$rtable_countries)
-d$rtable_country_count
-d$rtable_tCount
-
-dNew <- cbind(d['rtable_status_NUM'],d['rtable_kingdom_NUM'])
-str(dNew)
-corr.test(d[,c('rtable_status_NUM','rtable_name_NUM','rtable_kingdom_NUM','rtable_phylum_NUM','rtable_class_NUM',
-               'rtable_order_NUM','rtable_family_NUM','rtable_genus_NUM','rtable_size_NUM','rtable_threat_keywords_NUM','rtable_conservation_keywords_NUM',
-               'rtable_countries','rtable_country_count','rtable_tCount')])
-
-chisq.test(d) 
-str(d)
-# Order seems to predict conservation keywords (.55)
-# Status predicted fairly well by conservation keywords
-# Phylum predicts Kingdom at only 90%, showing us the matching rate of error
-scatterplotMatrix(d[,c('income','education','women')])
-
-
-# left
-d$ltable_country_count
-d$ltable_name_NUM <- as.numeric(d$ltable_name)
-d$ltable_genus_NUM <- as.numeric(d$ltable_genus)
-d$ltable_family_NUM <- as.numeric(d$ltable_family)
-d$ltable_ecology_NUM <- as.numeric(d$ltable_ecology)
-d$ltable_countries_NUM <- as.numeric(d$ltable_countries)
-d$ltable_pop_trend_NUM <- as.numeric(d$ltable_pop_trend)
-d$ltable_status_NUM <- as.numeric(d$ltable_status)
-
-corr.test(d[,c('ltable_country_count','ltable_name_NUM','ltable_genus_NUM',
-               'ltable_family_NUM','ltable_ecology_NUM','ltable_countries_NUM','ltable_pop_trend_NUM','ltable_status_NUM')])
-scatterplotMatrix(d[,c('income','education','women')])
+sjp.corr(dSubset)
 
 
+### Next, I'll make a model of some of our most predictive IV's
+m1 <- lm(status ~ popTrend, data = dSubset) # popTrend seems to have insufficient power (mostly decreasing, only a few increasing), F(1,89) = .429, p=.514
+modelSummary(m1,t=F)
 
-#################################################################
-# Determine which order, class, phylum has most endangered status
-#################################################################
-varDescribeBy(d$rtable_status_NUM,d$rtable_phylum_NUM)
-varDescribe(d$rtable_phylum_NUM)
-varDescribe(d$rtable_status_NUM)
+m2 <- lm(status ~ countryCount, data = dSubset) # approaching significance F(1,146) = 2.696, p = .103
+modelSummary(m2,t=F)
+
+m3 <- lm(status ~ threatCount, data = dSubset) 
+modelSummary(m3,t=F)
+
+modelSummary(m1,t=F)
+dSubset$popTrend
+
+# Now we'll look at categorical data - chisqare with...
+rtable_conservation_keywords
+
+
+
+#varsToLookAt <- c(d$ltable_status,d$ltable_pop_trend,d$ltable_country_count,d$ltable_ecology,d$rtable_tCount,d$rtable_kingdom,d$rtable_phylum,d$rtable_class,d$rtable_order,d$ltable_family)
+chisq.test(varsToLookAt) # yep
+
+
+# Individual chisq tests... these seem to work alright
+chisq.test(d$ltable_status,d$rtable_phylum)
+
+
+write.table(table(d$ltable_status,d$rtable_phylum),"eyooo.csv")
+
+chisq.test(d$ltable_status,d$rtable_class) # nope!
+chisq.test(d$ltable_status,d$rtable_order)
+chisq.test(d$ltable_status,d$ltable_family)
+chisq.test(d$ltable_status,d$genus)
+
+
 levels(d$rtable_phylum)
-levels(d$rtable_status)
 
+chisq.test(d$ltable_status,d$ltable_family) # yep
+chisq.test(d$ltable_pop_trend, d$ltable_status) # wow, doesn't predict... surprising
+chisq.test(d$rtable_kingdom,d$rtable_phylum) # sanity check successful
+chisq.test(d$ltable_country_count,d$rtable_kingdom) # cool, this is predictive
+chisq.test(d$ltable_pop_trend,d$rtable_kingdom) # nope
+chisq.test(d$ltable_pop_trend,d$rtable_phylum) # yep
+chisq.test(d$length,d$rtable_phylum) # yep
+chisq.test(d$length,d$ltable_status) # yep
+chisq.test(d$weight,d$ltable_status) # yep
+chisq.test(d$ltable_ecology,d$ltable_status) # nope
+chisq.test(d$rtable_tCount,d$ltable_status) # nope, not even close
+chisq.test(d$rtable_tCount,d$ltable_ecology) # yep
+chisq.test(d$rtable_tCount,d$rtable_phylum) # yep
+chisq.test(d$rtable_conservation_keywords,d$ltable_status) # yep
+chisq.test(d$rtable_conservation_keywords,d$rtable_phylum) # yep
+chisq.test(d$ltable_ecology,d$ltable_status) # nope
 
-#################################################################
-####### Run a model predicting status from most correlative vars
-#################################################################
-
-m <- 
-
-#############################################
-############## barplot code #################
-#############################################
-# FROM LAB 4
-# Plotting using barplots ####
-
-# Although we could technically use a scatterplot to graph the results,
-# the general convention is to create a barplot instead of a scatterplot.
-
-pY2 <- data.frame(Condition = c(0, 1))
-
-# need to generate the mean of predicted values for each condition
-# also need lower and upper standard error bounds for each condition
-# SE bars represent standard error of point estimates
-pY2 <- modelPredictions(m4, pY2)
-pY2
-
-# Note how the predicted values are just the means within each group
-varDescribeBy(d$Wk4IAT, d$Condition) 
-
-# make a Condition variable that has a descriptive name for labels
-d$ConditionStr <- varRecode(d$Condition,c(0,1),c('Control','Training'))
-pY2$ConditionStr <- varRecode(pY2$Condition,c(0,1),c('Control','Training'))
-
-# Starting plot, in which we plot bars
-plot <- ggplot(d, aes(x = ConditionStr, y = Predicted)) +
-  geom_bar(mapping = aes(fill = as.factor(ConditionStr)), # add colors and labels based on condition
-           data = pY2, 
-           stat = "identity", # using stat = "identity" because bars represent particular values in dataset
-           width = 0.5) 
-plot 
-
-# add the raw data points with jittering so we can see them
-plot <- plot + geom_point(data = d, aes(y = Wk4IAT, x = ConditionStr),colour='darkgrey',
-                          position = position_jitter(w = 0.1, h = 0.1))
-plot
-
-# add error bars
-plot <- plot + geom_errorbar(data = pY2, width=.25, # add error bars of width 0.25
-                             aes(y = Predicted, x = ConditionStr, # set error bar aesthethics: x & y variables
-                                 ymin = CILo, # define bottom (then top) of error bars
-                                 ymax = CIHi), stat="identity", width=0.75) # heights of bars rep values of data
-plot
-
-# finally, add labels, a title and remove the legend since it is unnecessary
-plot <- plot +labs(y = 'Week 4 IAT Score', x = 'Condition') + # set axis labels
-  ggtitle('Week 4 IAT by Condition') + # set graph title
-  theme_bw(base_size = 14) +  theme(legend.position="none") # remove the unnecessary legend
-plot
-
-# Everything at once (combining previous code into single plot)
-barplot <- ggplot(d, aes(x = ConditionStr, y = Predicted)) +
-  geom_bar(mapping = aes(fill = as.factor(ConditionStr)), data = pY2, stat = "identity", width = 0.5) +
-  geom_point(data = d, aes(y = Wk4IAT, x = ConditionStr),colour='darkgrey', 
-             position = position_jitter(w = 0.1, h = 0.1)) +
-  geom_errorbar(data = pY2, width=.25, aes(y = Predicted, x = ConditionStr, ymin = CILo, 
-                                           ymax = CIHi), stat="identity", width=0.75) + 
-  labs(y = 'Baseline IAT Score', x = 'Condition') + 
-  ggtitle('Baseline IAT by Condition') + theme_bw(base_size = 14) + theme(legend.position="none") 
-barplot
+#try to make one whole correlation table
+#varKeysToLookAt <- c('length','ltable_status','ltable_pop_trend','ltable_country_count','ltable_ecology','rtable_tCount','rtable_kingdom','rtable_phylum','rtable_class','rtable_order','ltable_family')
 
 
 
-#############################################
-############## histogram code ###############
-#############################################
-hist(d$Cats1, xlab = "Number of cats (in thousands)", main =
-       "Histogram of stray cat numbers at year one")
 
-hist(d$Cats5, xlab = "Number of cats (in thousands)", main =
-       "Histogram of stray cat numbers at year five")
 
 
 
